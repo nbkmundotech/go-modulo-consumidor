ce541f2f040ed42dc4521e56a8e90ae9916af92d
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-17 23:18:55 -0400

    Usar funcao MapaParaString

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nbkmundotech/go-modulo-consumidor
 
 go 1.16
 
-require github.com/nbkmundotech/go-modulo-biblioteca v0.3.0
+require github.com/nbkmundotech/go-modulo-biblioteca v0.4.0
--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 
 	"github.com/nbkmundotech/go-modulo-biblioteca/analisador"
+	"github.com/nbkmundotech/go-modulo-biblioteca/consolidar"
 )
 
 func main() {
 	var queryString = "?busca=termo&ordem=desc"
 	var queryMap map[string]string = analisador.Analisar(queryString)
 	fmt.Println(queryMap)
+
+	var queryStringRevertida string = consolidar.MapaParaString(queryMap)
+	fmt.Println(queryStringRevertida)
 }
